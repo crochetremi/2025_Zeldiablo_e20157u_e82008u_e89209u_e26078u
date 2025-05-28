d2e92ce21dedecb16e74df3ef465fafa906df68c
Rémi Crochet <remicrochet.310@orange.fr>
2025-05-28 14:32:42 +0200

    copie de la v0 dans un dossier à part

---
+++ documents/v0/src/gameLaby/laby/LabyDessin.java
@@ -0,0 +1,44 @@
+package gameLaby.laby;
+
+
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import moteurJeu.DessinJeu;
+import moteurJeu.Jeu;
+
+public class LabyDessin implements DessinJeu {
+
+    @Override
+    public void dessinerJeu(Jeu jeu, Canvas canvas) {
+
+        //Crée le jeu
+        LabyJeu laby = (LabyJeu) jeu;
+
+        //Crée le canva
+        final GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        // dessin fond
+        gc.setFill(Color.LIGHTGRAY);
+        gc.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
+
+        // dessin perso
+        gc.setFill(Color.RED);
+        Perso pj = laby.getLabyrinthe().pj;
+        double px = pj.x;
+        double py = pj.y;
+        //gc.fillRect(px * 20, py*20, 20, 20);
+        gc.fillArc(px*80, py*80, 80, 80, 0, 360, ArcType.ROUND);
+
+
+        for(int i = 0; i <laby.getLabyrinthe().getLength(); i++){
+            for(int j = 0; j < laby.getLabyrinthe().getLengthY(); j++){
+                if( laby.getLabyrinthe().getMur(i,j) == true ) {
+                    gc.setFill(Color.BLACK);
+                    gc.fillRect(i*80,j*80,80,80);
+                }
+            }
+        }
+    }
+}
---
+++ documents/v0/src/gameLaby/laby/MainLaby.java
@@ -0,0 +1,25 @@
+package gameLaby.laby;
+import moteurJeu.MoteurJeu;
+
+
+public class MainLaby {
+
+    public static void main(String[] args) {
+        int width = 800;
+        int height = 600;
+        int pFPS = 20;
+
+        // creation des objets
+        LabyJeu jeuLaby = new LabyJeu();
+        jeuLaby.init();
+
+        LabyDessin dessinArk = new LabyDessin();
+
+        // parametrage du moteur de jeu
+        MoteurJeu.setTaille(width,height);
+        MoteurJeu.setFPS(pFPS);
+
+        // lancement du jeu
+        MoteurJeu.launch(jeuLaby, dessinArk);
+    }
+}
